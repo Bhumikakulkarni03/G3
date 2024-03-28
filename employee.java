d5a7c7e549bae8fa9b0214b977719dd468cccc84
Bhumika <01fe22bca015@kletech.ac.in>
2024-03-28 20:18:38 +0530

    program to store employee details

---
+++ employee.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class employee {
+    int id;
+    String name;
+    int salary;
+    void read(){
+        System.out.println("Enter id name and salary of employee");
+        Scanner s=new Scanner(System.in);
+        id=s.nextInt();
+        name=s.next();
+        salary=s.nextInt();
+    }
+    void display(){
+        System.out.println("Id: "+id+"\nName: "+name+"\nSalary: "+salary);
+    }
+    public static void main(String[] args) {
+        employee e1=new employee();
+        e1.read();
+        e1.display();
+    }
+}
